14d1af7ba162be1dd09366399c50c97230064af0
grzes5003 <grzes.kulinski@gmail.com>
2021-06-24 23:45:56 +0200

    Changes to README.MD. Minor changes to client and server

--- server/main.c
+++ server/main.c
@@ -13,34 +13,6 @@
 #define SERV_PORT 5500
 #define RCV_BUFFSIZE  10000
 
-void srv_rcv_loop(int srvsockfd)
-{
-    int		n;
-    struct sockaddr_in6 cliaddr;
-    socklen_t	srcaddrlen;
-    char *rcvbuff = malloc(RCV_BUFFSIZE);
-    char		ipv6_human[INET6_ADDRSTRLEN+1];
-    time_t		ticks;
-
-    fprintf(stderr,"Waiting for clients ... \n");
-    for (;;) {
-        if( (n = recvfrom(srvsockfd, (void *) rcvbuff, RCV_BUFFSIZE, 0, (struct sockaddr *) &cliaddr, &srcaddrlen)) < 0 ){
-            fprintf(stderr,"recvfrom error : %s\n", strerror(errno));
-            exit(1);
-        }
-
-        inet_ntop(AF_INET6, &cliaddr.sin6_addr, ipv6_human, sizeof(ipv6_human));
-        printf("Received datagram from %s\n", ipv6_human);
-
-
-//        if( sendto(sockfd, mesg, strlen(mesg), 0, pcliaddr, len) < 0 ) {
-//            fprintf(stderr,"sendto error : %s\n", strerror(errno));
-//            //         continue;
-//            exit(1);
-//        }
-    }
-}
-
 int main() {
     sensor_t *sensor_arr;
 
