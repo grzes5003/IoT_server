04a0972b66f808e27765a13d77c79d4586f4ec18
grzes5003 <grzes.kulinski@gmail.com>
2021-06-24 21:25:52 +0200

    Implementation of control functions. Refactored common message types. Changes to README.md

--- server/common.h
+++ server/common.h
@@ -5,6 +5,13 @@
 
 typedef uint64_t token_t;
 
+typedef uint8_t in_msg_t;
+
+enum {
+    MSG_HELLO = 30,
+#define MSG_HELLO               MSG_HELLO
+};
+
 /* Common type to represent request type */
 typedef uint8_t in_req_t;
 
@@ -17,13 +24,12 @@ enum {
 #define REQ_SHUTDOWN            REQ_SHUTDOWN
     REQ_CHANGE_UPDATE_PRD = 3,
 #define REQ_CHANGE_UPDATE_PRD   REQ_CHANGE_UPDATE_PRD
-    REQ_BATTERY_STATUS = 5,
-#define REQ_BATTERY_STATUS      REQ_BATTERY_STATUS
 };
 
 typedef struct request_t {
     in_req_t req_type;
     token_t  token;
+    char *payload;
 } request_t;
 
 /* Common type to represent response type */
@@ -32,19 +38,14 @@ typedef uint8_t in_res_t;
 enum {
     RES_ACK = 10,
 #define RES_ACK                 RES_ACK
-    RES_OK = 11,
-#define RES_OK                  RES_OK
     RES_ERR = 12,
 #define RES_ERR                 RES_ERR
-    RES_READ_SENSOR = 13,
-#define RES_READ_SENSOR         RES_READ_SENSOR
-    RES_BATTERY_STATUS = 15,
-#define RES_BATTERY_STATUS      RES_BATTERY_STATUS
 };
 
 typedef struct response_t {
     in_res_t res_type;
     token_t  token;
+    char *payload;
 } response_t;
 
 #endif //SERVER_COMMON_H
--- server/control.h
+++ server/control.h
@@ -12,16 +12,16 @@ typedef struct sensor_t {
 #define SENSOR_ARR_SIZE 100
 
 /**
- * Add sensor to list of subscribed sensors
- * @param sensor_arr array of subscribed sensors
+ * Add sensor to linked list of subscribed sensors
+ * @param sensor_arr linked list of subscribed sensors
  * @param sensor to be added
  * @return status
  */
 int sens_add_remote(sensor_t *sensor_arr, sensor_t *sensor);
 
 /**
- * Remove sensor from list of subscribed sensors
- * @param sensor_arr array of subscribed sensors
+ * Remove sensor from linked list of subscribed sensors
+ * @param sensor_arr linked list of subscribed sensors
  * @param sensor to be removed
  * @return
  */
@@ -29,8 +29,22 @@ int sens_remove_remote(sensor_t *sensor_arr, sensor_t *sensor);
 
 int change_mode();
 
-request_t prepare_request(in_req_t req_type, token_t *token);
+/**
+ * Prepares request based on arguments
+ * @param req_type Type of request
+ * @param token Auth token
+ * @param payload Message to be included
+ * @return request_t pointer
+ */
+request_t *prepare_request(in_req_t req_type, const token_t *token, char *payload);
 
-response_t prepare_response(in_res_t res_type, token_t *token);
+/**
+ * Prepares request based on arguments
+ * @param res_type Type of request
+ * @param token Auth token
+ * @param payload Message to be included
+ * @return response_t pointer
+ */
+response_t *prepare_response(in_res_t res_type, const token_t *token, char *payload);
 
 #endif //SERVER_CONTROL_H
